ac734486b9a8f6c42821b1cf473f9bd7ef167788
Cristiano Leite <94697822+CristianosLeite@users.noreply.github.com>
2024-06-07 21:23:20 -0300

    moved files to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/app.{js,jsx,ts,tsx}", "./src/app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
